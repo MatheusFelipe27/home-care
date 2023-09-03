b221649b1aa25735d30ff5cf84849520f1d78a4c
Matheus Felipe <mfs5@cin.ufpe.br>
2023-09-03 17:10:27 -0300

    feat add Capacitor component

---
+++ src/components/capacitor/Capacitor.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Capacitor.scss'
+
+const Capacitor = ({btnName, name, img} : any) => {
+  return (
+    <>
+        <div className= {img==="research"? 'cardResearch' : 'cardFruit'}>
+            <button>{btnName}</button>
+            <span>{name}</span>
+        </div>
+    </>
+  )
+}
+
+export default Capacitor
