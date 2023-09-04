bff87e4b735bee79de9ad3928a120e4d2156810f
Matheus Felipe <mfs5@cin.ufpe.br>
2023-09-04 00:57:33 -0300

    add  new route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import Details from './pages/details/Details';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element= {<Login/>} />
           <Route path='/home' element={<Home/>} />
           <Route path ='/details' element = {<Details/>} />
+          <Route path = '/profile' element = {<Profile/>} />
         </Routes>  
       </Router>
     </>
