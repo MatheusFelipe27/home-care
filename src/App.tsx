b1730d299f8d26f76ee425bb3f5ce5769d9a1cea
Matheus Felipe <mfs5@cin.ufpe.br>
2023-09-03 21:49:16 -0300

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Login from './pages/login/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 import Home from './pages/home/Home';
+import Details from './pages/details/Details';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element= {<Login/>} />
           <Route path='/home' element={<Home/>} />
+          <Route path ='/details' element = {<Details/>} />
         </Routes>  
       </Router>
     </>
